e11d845ea0a8289b6d41e4dd0bcb626cf05074e6
carlosoli2011 <carlos.oli.2011@gmail.com>
2017-01-21 18:01:53 -0200

    jsf

--- src/main/java/br/com/dextraining/web/controller/LoginMB.java
+++ src/main/java/br/com/dextraining/web/controller/LoginMB.java
@@ -1,11 +1,17 @@
 package br.com.dextraining.web.controller;
 
+import java.io.IOException;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 import br.com.dextraining.domain.Usuario;
 import br.com.dextraining.domain.UsuarioService;
 
 @ManagedBean(name="loginMB")
+@SessionScoped
 public class LoginMB {
 
 	private Usuario usuario = new Usuario();
@@ -20,6 +26,11 @@ public class LoginMB {
 		this.usuario = usuario;
 	}
 	
+	public String logout(){
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		return "/login.jsf?faces-redirect=true";
+	}
+	
 	
 	
 	public boolean isLogado() {
@@ -30,8 +41,18 @@ public class LoginMB {
 		this.logado = logado;
 	}
 
-	public void login() {
+	public String login() {
 		this.logado = UsuarioService.logar(this.usuario);
+		if(this.logado){
+			ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+			try {
+				ec.redirect("/listar.jsf");
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			return null;
+		}
+		return null;
 	}
 	 
 }
